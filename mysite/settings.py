7ea4472e9efcbd318c359828876172921e7f1092
popoffww <popoffww@yandex.ru>
2024-04-29 17:43:08 +0600

    Добавление карты сайта

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+SITE_ID = 1
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     # Installed
     'blog.apps.BlogConfig',
     'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
