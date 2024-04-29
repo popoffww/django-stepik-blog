797a396816236e887edc61afb2b9a7b38f004ae1
popoffww <popoffww@yandex.ru>
2024-04-29 19:37:09 +0600

    Добавление поиска в блог

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,10 @@ from taggit.models import Tag
 from .forms import EmailPostForm, CommentForm
 from django.views.decorators.http import require_POST
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
+from .forms import EmailPostForm, CommentForm, SearchForm
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 
 # Представления на основе функций
 def posts_list(request, tag_slug=None):
@@ -117,3 +121,24 @@ def post_comment(request, post_id):
                   {'post': post,
                    'form': form,
                    'comment': comment})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
+
+    return render(request,
+                  'blog/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})
